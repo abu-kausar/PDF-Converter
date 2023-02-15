283317e3f4329990614d94529cb4bfa82654a7ac
abu-kausar <abukausar33@gmail.com>
2023-02-15 14:33:08 +0600

    project setup updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import LandingPage from './components/LandingPage/LandingPage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is a simple pdf converter application</h1>
-      <h2>This is update from kausar</h2>
-    </div>
+    <>
+      <LandingPage/>
+    </>
   );
 }
 
