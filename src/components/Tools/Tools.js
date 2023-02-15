e5a1e08929b7abcfffd645ce90c7c7c6e1830081
abu-kausar <abukausar33@gmail.com>
2023-02-15 22:53:59 +0600

    updated

--- src/components/Tools/Tools.js
+++ src/components/Tools/Tools.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Tools = () => {
     return (
         <div>
-            <h1>This is tools page extra</h1>
+            <h1>This is tools page extra2</h1>
         </div>
     );
 };
