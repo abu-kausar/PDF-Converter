6354533d00b4d68725b0f1f8dfcc4489cceac4d6
abu-kausar <abukausar33@gmail.com>
2023-02-15 16:11:28 +0600

    navbar and router added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import LandingPage from './components/LandingPage/LandingPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import About from '../About/About';
+import Help from '../Help/Help';
 import Home from '../Home/Home';
 import NavBar from '../NavBar/NavBar';
 import NotFound from '../NotFound/NotFound';
+import Tools from '../Tools/Tools';
 
 const LandingPage = () => {
     return (
@@ -12,6 +15,9 @@ const LandingPage = () => {
                 <Routes>
                     <Route exact path='/' element={<Home/>}/>
                     <Route exact path='/home' element={<Home/>}/>
+                    <Route exact path='/tools' element={<Tools/>}/>
+                    <Route exact path='/about' element={<About/>}/>
+                    <Route exact path='/help' element={<Help/>}/>
                     <Route path='/*' element={<NotFound/>}/>
                 </Routes>
             </Router>
