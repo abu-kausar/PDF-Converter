23e174d929478ec6640dc5ff72b1b2017a9e3eaf
abu-kausar <abukausar33@gmail.com>
2023-03-11 12:22:44 +0600

    warning updated

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import About from '../About/About';
 import Help from '../Help/Help';
 import Home from '../Home/Home';
